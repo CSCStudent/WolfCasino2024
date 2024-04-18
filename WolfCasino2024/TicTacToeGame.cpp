1762a028680a56d3f478f8a3950b69a08e33d99c
AnormaluserDrewF <167474141+AnormaluserDrewF@users.noreply.github.com>
2024-04-18 15:23:15 -0600

    Update TicTacToeGame.cpp

--- WolfCasino2024/TicTacToeGame.cpp
+++ WolfCasino2024/TicTacToeGame.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <ctime>
 #include "TicTacToeGame.h"
+#include <stdio.h>
+#include <sqlite3.h> 
+
+int main(int argc, char* argv[]) {
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+
+   rc = sqlite3_open("test.db", &db);
+
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+   } else {
+      fprintf(stderr, "Opened database successfully\n");
+   }
+   sqlite3_close(db);
+} //yes i added an sqlite3
 
 //defined namespace for project
 namespace CSC2034
